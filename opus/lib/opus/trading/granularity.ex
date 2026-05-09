76f3d0adea2695ac516c7bad7edb35a8ff61338c
jason <ja.schultz@gmail.com>
2026-05-08 19:05:51 -0600

    [fix/add_h4_to_opus_rust] Fixed compile time issues with the regime changes
    Ran into compile time issues with the previous H4 changes. So this PR fixes those issues.

--- opus/lib/opus/trading/granularity.ex
+++ opus/lib/opus/trading/granularity.ex
@@ -3,8 +3,12 @@ defmodule Opus.Trading.Granularity do
   Utility module for working with granularity strings. Central source of truth for
   valid granularities and related helper functions.
   """
+  @all ~w[M1 M15 H1 H4]
+  @mtf ~w[M15 H1 H4]
 
-  def mtf, do: ~w[M15 H1 H4]
-  def all, do: ~w[M1] ++ mtf()
-  def valid?(g), do: g in all()
+  defguard is_valid(g) when g in @all
+
+  def mtf, do: @mtf
+  def all, do: @all
+  def valid?(g), do: g in @all
 end
